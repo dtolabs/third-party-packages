e7178fe7e099e591f69357ae8c86a09d9d66a285
Anthony Shortland <anthony@dtosolutions.com>
2012-12-04 11:00:13 -0800

    Integrates the set of package repositories.
    Adds the ability to specify and iterate across the list of third-party package repositories. Uses the functions to implement creating and running build jobs as part of the deployment process.

--- lib/functions.sh
+++ lib/functions.sh
@@ -14,4 +14,17 @@
 # Your functions declared here.
 #
 
+# Set of helper functions to manage access to the file specifying the set of packages to be managed.
+list-third-party-package-repositories() {
+  grep -v "^#" $RERUN_MODULE_DIR/etc/packages
+}
 
+get-third-party-package-repository-owner() {
+  [[ $# -ne 1 ]] && rerun_die "usage: \"get-third-party-repository-owner repository\""
+  echo "$1" | cut -d/ -f1
+}
+
+get-third-party-package-repository-name() {
+  [[ $# -ne 1 ]] && rerun_die "usage: \"get-third-party-package-repository-name repository\""
+  echo "$1" | cut -d/ -f2
+}
