32408fd61ef5f77ac340164839060e33a037c317
Anthony Shortland <anthony@dtosolutions.com>
2012-12-04 11:35:26 -0800

    Updates the modules headers and options to work with the latest version of stubbs and completes the deploy workflow to the point of creating and kicking off the build jobs.

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,7 +1,6 @@
-# 
-# Shell functions for third-party packaging commands
+# Shell functions for the third-party-packages module.
+#/ usage: source RERUN_MODULE_DIR/lib/functions.sh command
 #
-
 
 # Read rerun's public functions
 . $RERUN || {
@@ -9,10 +8,24 @@
     return 1
 }
 
+# Check usage. Argument should be command name.
+[[ $# = 1 ]] || rerun_option_usage
 
-# ----------------------------
-# Your functions declared here.
+# Source the option parser script.
 #
+if [[ -r $RERUN_MODULE_DIR/commands/$1/options.sh ]] 
+then
+    . $RERUN_MODULE_DIR/commands/$1/options.sh || {
+        rerun_die "Failed loading options parser."
+    }
+fi
+
+# - - -
+# Your functions declared here.
+# - - -
+
+# Yuck! Needed to make Yum behave on CentOS 6.
+export LC_ALL=C
 
 # Set of helper functions to manage access to the file specifying the set of packages to be managed.
 list-third-party-package-repositories() {
